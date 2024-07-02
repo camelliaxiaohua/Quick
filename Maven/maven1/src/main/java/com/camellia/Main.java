bd4242581c7020597763512bc26ad22d4951be51
Camellia.xiaohua <2421195723@qq.com>
2024-07-02 16:39:00 +0800

    Maven.md

---
+++ Maven/maven1/src/main/java/com/camellia/Main.java
@@ -0,0 +1,11 @@
+package com.camellia;
+
+/**
+ * @Datetime: 2024/7/2下午1:58
+ * @author: Camellia.xioahua
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Maven");
+    }
+}
---
+++ Maven/maven1/src/test/java/DemoTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * @Datetime: 2024/7/2下午3:26
+ * @author: Camellia.xioahua
+ */
+public class DemoTest {
+    @Test
+    public void test() {
+        System.out.println("Test1....");
+    }
+}
